7bd842e82e67e9e711121e93ea9be45535109da3
jessemos <yeashai.moskovitch@gmail.com>
2020-01-17 04:30:56 +0200

    UI Changes: Area Info + STC Shortcuts in New Editor Card, 1 col-9 & 1 col-3

--- Resources/js/app.js
+++ Resources/js/app.js
@@ -1,44 +1,8 @@
 window.addEventListener("DOMContentLoaded", function () {
-
-    function returnHTMLSnippet(htmlSnippet) {
-        var httpRequest;
-        httpRequest = new XMLHttpRequest();
-
-        if (!httpRequest) {
-            alert('Giving up :( Cannot create an XMLHTTP instance');
-            return false;
-        }
-        httpRequest.onreadystatechange = function(){
-            if (httpRequest.readyState === XMLHttpRequest.DONE) {
-                if (httpRequest.status === 200) {
-                    alert(httpRequest.responseText);
-                } else {
-                    alert('There was a problem with the request.');
-                }
-            }
-        };
-
-        httpRequest.open('GET', '../Resources/js/HTML_Snippetes/' + htmlSnippet + '.html',false);
-        httpRequest.send();        
-    }
-    returnHTMLSnippet("DSInfo");
-    var dsInfo = `
-    <div class="row">
-    <div class="col">
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <label class="input-group-text" data-toggle="tooltip" for="inputGroupSelectPriority"
-                    title="Select from dropdown to change">Priority:</label>
-            </div>
-            <select class="custom-select" id="inputGroupSelectPriority">
-                <option selected>Choose...</option>
-                <option value="1">One</option>
-                <option value="2">Two</option>
-                <option value="3">Three</option>
-            </select>
-        </div>
-    </div>
-    <div class="col">
+    var dsInfo =
+`
+<div class="row pb-3">
+    <div class="col-5">
         <div class="input-group">
             <div class="input-group-prepend">
                 <label class="input-group-text" data-toggle="tooltip" for="SearchAreaByName"
@@ -47,14 +11,14 @@ window.addEventListener("DOMContentLoaded", function () {
                 </label>
             </div>
             <select class="custom-select" id="SearchAreaByName">
-                <option selected>Search by Area Name...</option>
+                <option selected>Select...</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
             </select>
         </div>
     </div>
-    <div class="col">
+    <div class="col-2">
         <div class="input-group">
             <div class="input-group-prepend">
                 <label class="input-group-text" data-toggle="tooltip" for="SearchAreaByID"
@@ -63,22 +27,36 @@ window.addEventListener("DOMContentLoaded", function () {
                 </label>
             </div>
             <select class="custom-select" id="SearchAreaByID">
-                <option selected>Search by Area ID...</option>
+                <option selected>Select...</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
             </select>
         </div>
     </div>
-    <div class="col">
+    <div class="col-2">
+        <div class="input-group">
+            <div class="input-group-prepend">
+                <label class="input-group-text" data-toggle="tooltip" for="inputGroupSelectPriority"
+                    title="Select from dropdown to change">Priority:</label>
+            </div>
+            <select class="custom-select" id="inputGroupSelectPriority">
+                <option selected>Select...</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select>
+        </div>
+    </div>
+    <div class="col-3">
         <div class="input-group">
             <div class="input-group-prepend">
                 <label class="input-group-text" data-toggle="tooltip" for="inputGroupSelectDS"
-                    title="Select from dropdown to change">Relevant Data
-                    Source:</label>
+                    title="Select from dropdown to change">Default Data
+                    Source #:</label>
             </div>
             <select class="custom-select" id="inputGroupSelectDS">
-                <option selected>Choose...</option>
+                <option selected>Select...</option>
                 <option value="1">One</option>
                 <option value="2">Two</option>
                 <option value="3">Three</option>
@@ -86,10 +64,41 @@ window.addEventListener("DOMContentLoaded", function () {
         </div>
     </div>
 </div>
+<div class="row">
+    <div class="col">
+        <button class="btn btn-primary text-center w-100" onclick='google.script.run.openPendingSA()' type="button">Open
+            Editor
+            Alerts
+            Page
+        </button>
+    </div>
+    <div class="col">
+        <button class="btn btn-primary text-center w-100" onclick='google.script.run.openPendingSTC()'
+            type="button">Open
+            Editor
+            STCs
+            Page
+        </button>
+    </div>
+    <div class="col">
+        <button class="btn btn-primary text-center w-100" onclick='google.script.run.openLinesEditor()'
+            type="button">Open
+            Editor
+            Lines Page
+        </button>
+    </div>
+    <div class="col">
+        <button class="btn btn-primary text-center w-100" onclick='google.script.run.openMoovitAppArea()' type="button">
+            Open
+            Moovitapp
+            area Page
+        </button>
+    </div>
+</div>
     `
     alert("Ready for more coding!");
     var newEditor = [];
-    newEditor.push(document.getElementById("collapseNewEditorCardHeader"));
+    newEditor.push(document.getElementById("NewEditorHeader"));
     newEditor[0].innerHTML = dsInfo;
     console.log(newEditor[0].nodeType);
 });
